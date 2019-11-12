150ab13024e6df5b446f68efd71ff9faea07f056
kudotaka0421 <zmk1n421@gmail.com>
2019-11-12 14:35:45 +0900

    rails g model location

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_12_052143) do
+ActiveRecord::Schema.define(version: 2019_11_12_053258) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -34,6 +34,12 @@ ActiveRecord::Schema.define(version: 2019_11_12_052143) do
   end
 
   create_table "languages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "locations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
