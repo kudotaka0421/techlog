12be152da496b2dca9ec1e8fbce9f3d4aa0f0b7c
kudotaka0421 <zmk1n421@gmail.com>
2019-11-25 01:41:26 +0900

    :numericalityを用いてrateにバリデーションを実装

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,7 @@ class Review < ApplicationRecord
   belongs_to :school
   belongs_to :user
   validates :rate, :content_good, :content_bad, presence: true
+  validates :rate,:numericality => {
+    :less_than => 5,
+  }
 end
