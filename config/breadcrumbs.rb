4a65ff42a5f3df3b26ab12f87ef555c8ae73fe65
kudotaka0421 <zmk1n421@gmail.com>
2019-11-27 17:31:54 +0900

    review#edit, review#destroyのパンくず実装

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -7,6 +7,20 @@ crumb :school_show do
   parent :top
 end
 
+crumb :review_show do
+  @school = School.find(params[:school_id])
+  link "#{@school.name} (レビュー投稿)"
+  parent :top
+end
+
+crumb :review_edit do
+  @school = School.find(params[:school_id])
+  link "#{@school.name} (レビュー編集)"
+  parent :top
+end
+
+
+
 crumb :location_show do
   @location = Location.find(params[:id])
   link "#{@location.name}のスクール", location_path
