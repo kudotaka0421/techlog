1c11a405e7acaa93cc581b1340b2f64ea674c986
kudotaka0421 <zmk1n421@gmail.com>
2019-11-27 16:19:43 +0900

    ログイン中ユーザーが自身のreviewを削除できるように実装

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,9 @@
 class ReviewsController < ApplicationController
 
   def index
-    @review = Review.new
-    @reviews = @school.reviews.includes(:user)
+    @reviews = Review.order("id ASC")
+    # @review = Review.new
+    # @reviews = @school.reviews.includes(:user)
   end
 
   def new
@@ -12,7 +13,7 @@ class ReviewsController < ApplicationController
 
   def create
     @school = School.find(params[:school_id])
-    @review = Review.create(review_params)
+    @review = Review.new(review_params)
     if @review.save
       redirect_to school_path(@school), notice: 'レビューが送信されました'
     else
@@ -27,26 +28,37 @@ class ReviewsController < ApplicationController
   end
 
   def update
-    @school = School.find(school_id: params[:school_id])
+    @school = School.find(params[:school_id])
     @review = Review.find(params[:id])
-    if @review.user_id == current_user.id
-      @review.update(review_params)
+    if @review.user_id == current_user.id && @review.update(review_params)
       redirect_to school_path(@school), notice: "編集が完了しました"
     else
-      flash.now[:alert] = '入力内容に不備があります。'
-      render :new
+      flash.now[:alert] = '編集内容に不備があります'
+      render :edit
     end
   end
 
   def destroy
+    @school = School.find(params[:school_id])
+    @review = Review.find(params[:id])
+    if @review.user_id == current_user.id && @review.destroy
+      redirect_to school_path(@school), notice: "削除が完了しました"
+    else
+      flash.now[:alert] = '削除できませんでした'
+      redirect_to school_path(@school)
+    end
   end
 
 
   private
 
+  # def review_params
+  #   params.require(:review).permit(:rate, :content_good, :content_bad, :school_id, :user_id).merge(user_id: current_user.id)
+  # end
+
   def review_params
-    params.require(:review).permit(:rate, :content_good, :content_bad, :schoo_id)
-    .merge(user_id: current_user.id)
+    params.require(:review).permit(:rate, :content_good, :content_bad, :school_id)
+    .merge(school_id: params[:school_id],user_id: current_user.id)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_22_095458) do
+ActiveRecord::Schema.define(version: 2019_11_26_123310) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
